6f17289ea68b345bfbefe5cf895fdd2456ca2cf0
hayoub <ayoub.habibes@ynov.com>
2024-11-15 11:35:10 +0100

    added card and db.json

--- app.py
+++ app.py
@@ -4,55 +4,66 @@ import zipfile
 from automations.a1.main import run
 from apscheduler.schedulers.background import BackgroundScheduler
 from automations.a1.main import run  # Importation des scripts d'automatisation
-
+import json
 import threading
 import os
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open('db.json', 'r') as file:
+        db_data = json.load(file)
+    return render_template('index.html', db=db_data)
+
 
 @app.route('/a1', methods=['GET', 'POST'])
 def a1():
+    with open('db.json', 'r') as file:
+        db_data = json.load(file)
+
     if request.method == 'POST':
         user_input = request.form.get('content')
-        
+
         if user_input is None:
-            return "No input received", 400  # Retourne un message d'erreur si aucun input n'est reçu
+            # Retourne un message d'erreur si aucun input n'est reçu
+            return "No input received", 400
 
         thread = threading.Thread(target=run, args=(user_input,))
         thread.start()
-        
-        return redirect('/a1')  # Retourne toujours une réponse en chaîne de caractères
+
+        # Retourne toujours une réponse en chaîne de caractères
+        return redirect('/a1')
     else:
         pdf_list = os.listdir('automations/a1/downloads')
-        return render_template('a1/interface.html', pdf_list=pdf_list)
+        return render_template('a1/interface.html', db=db_data)
 
 
 @app.route('/a1/get_pdfs')
 def get_pdfs():
     # Obtenir la liste des fichiers PDF dans le dossier
-    pdf_files = [f for f in os.listdir('automations/a1/downloads') if f.endswith('.pdf')]
+    pdf_files = [f for f in os.listdir(
+        'automations/a1/downloads') if f.endswith('.pdf')]
     return jsonify(pdf_files)
 
+
 DOWNLOAD_FOLDER = "automations/a1/downloads"
+
 
 @app.route('/a1/get_folder')
 def get_folder():
     # Crée un objet BytesIO pour stocker le fichier ZIP en mémoire
     zip_stream = io.BytesIO()
-    
+
     # Création du fichier ZIP en mémoire
     with zipfile.ZipFile(zip_stream, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(DOWNLOAD_FOLDER):
             for file in files:
                 # Ajoute chaque fichier du dossier dans le ZIP
-                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), DOWNLOAD_FOLDER))
-    
+                zipf.write(os.path.join(root, file), os.path.relpath(
+                    os.path.join(root, file), DOWNLOAD_FOLDER))
+
     # Repositionner le pointeur au début du BytesIO pour le téléchargement
     zip_stream.seek(0)
 
@@ -69,15 +80,12 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
-
 # def a2():
 #     pass
 # # Configurer et démarrer le planificateur
 # scheduler = BackgroundScheduler()
 # scheduler.add_job(automated_task, 'interval', hours=12)
 # scheduler.start()
-
 
 
 if __name__ == '__main__':
