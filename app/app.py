1bbb6733de7c5a696de300bfd5d7128880d01a58
mastermetria <admin@metria.fr>
2024-11-27 00:43:08 +0100

    add downloads and processed files with their templates xlsx for production

--- app/app.py
+++ app/app.py
@@ -32,6 +32,30 @@ with open('db.json', 'r') as file:
         db_data = json.load(file)
         automations = db_data.get('automations')
 
+
+# Route pour afficher la page admin
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    # Charger le fichier JSON
+    with open('db.json', 'r') as f:
+        data = json.load(f)
+
+    if request.method == 'POST':
+        # Récupérer les données modifiées depuis le formulaire
+        updated_data = request.form.get('updated_data')
+        
+        try:
+            # Sauvegarder les nouvelles données dans le fichier JSON
+            with open('db.json', 'w') as f:
+                json.dump(json.loads(updated_data), f, indent=4)
+            return jsonify({"success": True, "message": "Données sauvegardées avec succès."})
+        except Exception as e:
+            return jsonify({"success": False, "message": f"Erreur : {str(e)}"}), 400
+
+    # Renvoyer la page HTML avec les données actuelles
+    return render_template('admin.html', data=json.dumps(data, indent=4))
+
+
 # Page de connexion
 @app.route('/login', methods=['GET', 'POST'])
 def login():
